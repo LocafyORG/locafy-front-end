922e319886ae2eeee1c7bc1d488186b10ec360ac
Matthieu Constant <91146150+MattConstant@users.noreply.github.com>
2024-10-23 21:09:21 -0400

    added react router dom and created basic pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/Home.tsx';
+import Locations from './pages/Locations/Locations.tsx';
+import Productions from './pages/Productions/Productions.tsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Hello</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/locations" element={<Locations/>} />
+      <Route path="/Productions" element={<Productions/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
