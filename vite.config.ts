8ae9ff9d756055ebabc4abf023a68246623180dc
Seiji <akakabeseiji0@gmail.com>
2024-11-23 16:54:08 -0500

    Fix linter problem where it cannot find the aliased imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
 	resolve: {
     alias: {
-      '@': '/src',
-			'@components': '/src/components',
-			'@layouts': '/src/layouts',
-			'@pages': '/src/pages',
-			'@constants': '/src/constants',
+      "@": "/src",
+			"@components": "/src/components",
+			"@layouts": "/src/layouts",
+			"@pages": "/src/pages",
+			"@constants": "/src/constants",
+			"@assets": "/src/assets",
     },
   },
+  plugins: [react()],
 })
