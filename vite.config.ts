ae1d6079765dc98596f1cee34726165b76959f2d
Seiji <akakabeseiji0@gmail.com>
2024-11-23 09:10:49 -0500

    Setup path aliases
    Setup path aliases to avoid relative path imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+	resolve: {
+    alias: {
+      '@': '/src',
+			'@components': '/src/components',
+			'@layouts': '/src/layouts',
+			'@pages': '/src/pages',
+			'@constants': '/src/constants',
+    },
+  },
 })
