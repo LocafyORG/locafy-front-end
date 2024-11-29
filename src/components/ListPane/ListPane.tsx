ea6f3ae3426bb485805bf22dd90a661fe9b44e95
Seiji <akakabeseiji0@gmail.com>
2024-11-28 22:31:40 -0500

    Create a flexible list pane component.

---
+++ src/components/ListPane/ListPane.tsx
@@ -0,0 +1,74 @@
+import { ReactNode, useState, useEffect } from "react"
+import { Link } from "react-router"
+import { CContainer, CTableBody, CTableHead, CTable, CTableRow, CTableHeaderCell, CTableDataCell } from '@coreui/react'
+import './ListPane.css'
+
+type ColumnData = string;
+type ListPaneRowData = {[key: string]: ColumnData };
+	
+interface ListPaneProps {
+	data: ListPaneRowData[],
+	colNameArrangement: { [key: string]: string }
+}
+
+export default function ListPane({ data, colNameArrangement }: ListPaneProps) {
+	const [keys] = useState<string[]>(Object.keys(colNameArrangement));
+	const [headRowNames, setHeadRowNames] = useState<string[]>([]);
+
+	useEffect(() => {
+		// The `Object.key()` and `Object.values()` change the arrangement of the elements.
+		// Using these functions for `keys` and `headRowNames` respectively may cause a misalignment.
+		const names = keys.map((key) => colNameArrangement[key]);
+		setHeadRowNames(names);
+	}, [keys]);
+
+	function processColData(dataArray: ListPaneRowData, key: string): ReactNode {
+		let colData: string | undefined = dataArray[key]
+
+		if (!colData) return <>{""}</>
+
+		if (colData.length > 2 && colData.startsWith("`") && colData.endsWith("`")) {
+			const regex = /\[([^\]]+)\]\(([^)]+)\)/;
+			const match = colData.match(regex);
+			const label = match?.[1];
+			const url = match?.[2];
+
+			if (!label || !url) {
+				return <h1>Come on man!</h1>
+			}
+
+			return <Link to={url}>{label}</Link>
+		}
+
+		return <>{colData}</>
+	}
+
+	return <>
+		<CContainer fluid className="list-pane">
+			<CTable>
+				<CTableHead>	
+					{/* Head Row */}
+					<CTableRow>
+						{headRowNames.map((name, colKey) => (
+							<CTableHeaderCell key={colKey}>{name}</CTableHeaderCell>
+						))}
+					</CTableRow>
+				</CTableHead>	
+
+				<CTableBody>
+					{/* Data Rows */}
+					{data.map((d, rowId) => (
+						<CTableRow key={rowId}>
+							{keys.map((key, colId) => (
+								<CTableDataCell key={colId}>
+									{processColData(d, key)}
+								</CTableDataCell>
+							))}	
+						</CTableRow>
+					))}
+				</CTableBody>
+
+			</CTable>
+		</CContainer>
+	</>
+}
--- src/pages/dashboard/contacts/Contacts.tsx
+++ src/pages/dashboard/contacts/Contacts.tsx
@@ -1,9 +1,43 @@
+import { useState } from "react"
+import ListPane from '@components/ListPane/ListPane'	
 import './Contacts.css'
 
-function Contacts() {
+/**
+ * `CTable` docs: `https://coreui.io/react/docs/components/table/`
+ */
+export default function Contacts() {
+	const [contacts] = useState<{[key:string]:string}[]>([
+		{
+			// Good example
+			name: "Doge",
+			telNum: "+1-(800)-858-0085",
+			email: "doge@cheemail.com",
+			locationId: "`[4](/dashboard/locations?user=4)`"
+		},
+		{
+			name: "Cheems",
+			telNum: "+1-(800)-858-0085",
+			age: "22",		// Ignored because 'age' is not specified in 'colNameArrangement'
+			email: "cheems_is_here@cheemail.com",
+			// Location is missing here so is, therefore, not rendered
+		},
+		{
+			name: "Kyle",
+			telNum: "+1-(800)-858-0085",
+			email: "doge@cheemail.com",
+			locationId: "`[Bad Link]()`"
+		},
+	]);
+
   return <>
 		<h1>Contacts</h1>
+		<ListPane data={contacts} colNameArrangement={{
+				name: "Name",
+				telNum: "Phone",
+				email: "Email",
+				locationId: "Locations",
+			}} />
 	</>
 }
 
-export default Contacts;
+
