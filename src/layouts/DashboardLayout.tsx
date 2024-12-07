2c867cd3913d859ab951028de5583ad7a7f12c22
Seiji <akakabeseiji0@gmail.com>
2024-12-06 19:11:03 -0500

    Fix DashboardLayout indentation

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -8,45 +8,45 @@ import { isAuthenticated } from '@api/auth/authTokenApi'
 import '@styles/layouts/DashboardLayout.scss'
 
 export default function DashboardLayout() {
-	const location = useLocation();
-	const [auth, setAuth] = useState<boolean>(isAuthenticated());
-	const [navButtons] = useState([
-		{
-			label: "Productions",
-			coreUiIcon: cilCamera,
-			to: ROUTES.PRODUCTIONS,
-		},
-		{
-			label: "Locations",
-			coreUiIcon: cilLocationPin,
-			to: ROUTES.LOCATIONS,
-		},
-		{
-			label: "Contacts",
-			coreUiIcon: cilContact,
-			to: ROUTES.CONTACTS,
-		},
-		{
-			label: "Calendar",
-			coreUiIcon: cilCalendar,
-			to: ROUTES.CALENDAR,
-		},
-	]);
+  const location = useLocation();
+  const [auth, setAuth] = useState<boolean>(isAuthenticated());
+  const [navButtons] = useState([
+    {
+      label: "Productions",
+      coreUiIcon: cilCamera,
+      to: ROUTES.PRODUCTIONS,
+    },
+    {
+      label: "Locations",
+      coreUiIcon: cilLocationPin,
+      to: ROUTES.LOCATIONS,
+    },
+    {
+      label: "Contacts",
+      coreUiIcon: cilContact,
+      to: ROUTES.CONTACTS,
+    },
+    {
+      label: "Calendar",
+      coreUiIcon: cilCalendar,
+      to: ROUTES.CALENDAR,
+    },
+  ]);
 
-	useEffect(() => {
-		setAuth(isAuthenticated());		// Trigger recomposition every time the path changes within the dashboard
-	}, [location]);
+  useEffect(() => {
+    setAuth(isAuthenticated());    // Trigger recomposition every time the path changes within the dashboard
+  }, [location]);
 
-	return auth ?
-		<div className="dashboard-layout">
-			<Sidebar buttons={navButtons} />
-			<div className="content-container">
-				<UserProfile />
-				<div className="content">
-					<Outlet />
-				</div>
-			</div>
-  	</div>
-		:
-		<Navigate to={ROUTES.LOGIN} replace />
+  return auth ?
+    <div className="dashboard-layout">
+      <Sidebar buttons={navButtons} />
+      <div className="content-container">
+        <UserProfile />
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+    :
+    <Navigate to={ROUTES.LOGIN} replace />
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: "modern",
-			},
-		},
-	},
-	resolve: {
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+      },
+    },
+  },
+  resolve: {
     alias: {
-			"@components": "/src/components",
-			"@layouts": "/src/layouts",
-			"@pages": "/src/pages",
-			"@constants": "/src/constants",
-			"@assets": "/src/assets",
+      "@components": "/src/components",
+      "@layouts": "/src/layouts",
+      "@pages": "/src/pages",
+      "@constants": "/src/constants",
+      "@assets": "/src/assets",
       "@api": "/src/services/api",
       "@styles": "/src/styles",
-			"@hooks": "/src/hooks",
+      "@hooks": "/src/hooks",
     },
   },
   plugins: [react()],
@@ -32,4 +32,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
