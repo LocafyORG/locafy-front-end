a998a59b3a94b8c838f965d0e8ec3fb48b553a81
Seiji <akakabeseiji0@gmail.com>
2024-12-12 11:04:14 -0500

    lint

--- src/services/api/interfaces/Contacts.ts
+++ src/services/api/interfaces/Contacts.ts
@@ -1,20 +1,20 @@
 export interface Contact {
-    contactId?: string;
-    name: string;
-    phone: string;
-    email: string;
-    notes: string;
-    uploadedById: string;
-    locationIds: string[];
-    nonFilmingIds: string[];
-    uploadedAt: string;
-    lastUpdated: string;
-  }
-  
-  export interface ContactInput {
-    name: string;
-    phone: string;
-    email: string;
-    notes: string;
-    assocLocationIds: string[];
-  }
+  contactId?: string;
+  name: string;
+  phone: string;
+  email: string;
+  notes: string;
+  uploadedById: string;
+  locationIds: string[];
+  nonFilmingIds: string[];
+  uploadedAt: string;
+  lastUpdated: string;
+}
+
+export interface ContactInput {
+  name: string;
+  phone: string;
+  email: string;
+  notes: string;
+  assocLocationIds: string[];
+}
