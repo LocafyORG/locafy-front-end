76f68b2543c2158f0052f0ff61b7b7928378a05b
Seiji <akakabeseiji0@gmail.com>
2024-12-19 17:23:55 -0500

    Add providers for Tanstack query and Google Maps

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,15 @@ import "@coreui/coreui/dist/css/coreui.min.css";
 import "material-icons/iconfont/material-icons.css";
 import "@styles/index.scss";
 import App from "./App.tsx";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <APIProvider apiKey={import.meta.env.VITE_PUBLIC_GOOGLE_MAPS_API_KEY}>
+      <App />
+    </APIProvider>
+  </QueryClientProvider>,
+);
